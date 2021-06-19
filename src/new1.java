7f808e18596b925bbafc02d6f55e8cfc4dc0f572
annakrolik <84658264+annakrolik@users.noreply.github.com>
2021-06-19 10:06:58 +0200

    switch class

---
+++ src/new1.java
@@ -0,0 +1,11 @@
+public class new1 {
+    public static void main(String[] args) {
+
+        int x =3;
+        switch (x){
+            case 1: System.out.println("to nie jest 3");
+            case 2: System.out.println("to nie jest 3");
+            case 3: System.out.println("to jest 3");
+        }
+    }
+}
