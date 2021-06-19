064044a2fea2db5e634b4cacb7bc2b951c327c47
annakrolik <84658264+annakrolik@users.noreply.github.com>
2021-06-19 11:05:38 +0200

    int/for

--- src/forclass.java
+++ src/forclass.java
@@ -1,12 +1,18 @@
-public class forclass {                         // deklaracja klasy
-    public static void main(String[] args) {     // deklaracja metody
+import org.w3c.dom.ls.LSOutput;
 
-        for (int i = 0; i < 15; i++){
-            if (i == 7){
-                continue;
-            }
-            System.out.println("Ania jest super" +i);
+public class forclass {                         //
+    public static void main(String[] args) {
 
-        }
+        int[] tablica = { 1,2,3,4,5 };
+        // System.out.println(tablica [2]);
+
+        for (int i =0; i < tablica.length; i++)
+            System.out.println(tablica[i]);
+
+
+
     }
 }
+
+
+
